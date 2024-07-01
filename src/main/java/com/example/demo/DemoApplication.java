4e8aeb80725b2cf281b178dfd812ae3be05f30a4
sam <sam.snowball@hotmail.co.uk>
2024-07-01 09:27:20 +0100

    Added max connection pool, tested and working

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,8 +9,13 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger; 
+import org.slf4j.LoggerFactory; 
+
 @SpringBootApplication
 public class DemoApplication {
+
+	Logger logger  = LoggerFactory.getLogger(DemoApplication.class); 
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -26,10 +31,20 @@ public class DemoApplication {
 		public String myMethod(){
 			String query = "SELECT * FROM trip";
 			List<Trip> trips = jdbcTemplate.query(query, new TripRowMapper());
+			trips.stream().forEach(t -> logger.info("trip" + t.toString()));
 
-			trips.stream().forEach(t -> System.out.println("trip" + t.toString()));
-
+			logger.info("Executing in:" + Thread.currentThread().getName());
 			return "Sam";
+		}
+
+		@GetMapping("/helloworld2")
+		public String myMethod2(){
+			String query = "SELECT * FROM trip";
+			List<Trip> trips = jdbcTemplate.query(query, new TripRowMapper());
+			trips.stream().forEach(t -> logger.info("trip" + t.toString()));
+
+			logger.info("Executing in:" + Thread.currentThread().getName());
+			return "Sam 2";
 		}
 	}
 
--- src/main/java/com/example/demo/SpringJdbcConfig.java
+++ src/main/java/com/example/demo/SpringJdbcConfig.java
@@ -7,9 +7,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-@Configuration
-@ComponentScan("com.example.demo")
+//@Configuration
+//@ComponentScan("com.example.demo")
 public class SpringJdbcConfig {
+
+    /* 
     @Bean
     public DataSource mysqlDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
@@ -20,4 +22,5 @@ public class SpringJdbcConfig {
 
         return dataSource;
     }
+    */
 }
